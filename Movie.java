d42d724bb4d1a1b6ee8b2072234f1b4c98f3ca3f
himedsoso <himed.abidar@gmail.com>
2023-02-27 16:43:37 +0100

    first commit

---
+++ Movie.java
@@ -0,0 +1,21 @@
+class Movie {
+
+	public static void main(String[] args) {
+	
+		String title = "Indiana Jones et la derniere croisade";
+		System.out.println(title);
+		
+		boolean look = false;
+		System.out.println(look);
+		
+		int year = 1989;
+		System.out.println(year);
+		
+		double n1 = 8.2;
+		System.out.println(n1);
+		
+		
+		
+	}
+
+}
